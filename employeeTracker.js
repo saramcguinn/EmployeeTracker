ccddfb6466fd61fb4d568786fdb56c44c40e7637
sara.mcguinn <sara.mcguinn@gmail.com>
2020-09-26 21:59:17 -0500

    start function

--- employeeTracker.js
+++ employeeTracker.js
@@ -20,9 +20,72 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("So far so good")
     // run the start function after the connection is made to prompt the user
-    //start();
+    start();
 });
 
-// function start() {}
+// ask "What would you like to do?"
+// choices: 
+//      View All Employees
+//      View All Employees By Department
+//      View All Employees by MAnager
+//      Add Employee
+//      Update Employee Role
+//      extra: Remove Employee
+//      extra: Update Employee Manager
+
+function start() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'start',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'View All Employees By Department',
+                'View All Employees By Manager',
+                'Add Employee',
+                'Update Employee Job'
+            ]
+        }
+    ])
+        .then(answers => {
+            if (answers.start === 'View All Employees') {
+                console.log("you chose view all empls");
+                // viewAllEmpls();
+            } else if (answers.start === 'View All Employees By Department') {
+                console.log("you chose view empls by dept");
+                // viewEmplsByDept();
+            } else if (answers.start === 'View All Employees By Manager') {
+                console.log("you chose view empls by manager");
+                //viewEmplsByManager();
+            } else if (answers.start === 'Add Employee') {
+                console.log("you chose add employee");
+                // addEmpl();
+            } else if (answers.start === 'Update Employee Job') {
+                console.log("you chose update employee job");
+                //updateEmplJob();
+            }
+        })
+}
+
+// function viewAllEmpls() {
+
+// }
+
+// function viewEmplsByDept() {
+//     let emplsByDeptArr = [connection.query("SELECT")]
+//     console.table(emplsByDeptArr);
+// }
+
+// function viewEmplsByManager() {
+
+// }
+
+// function addEmpl() {
+
+// }
+
+// function updateEmplJob() {
+
+// }
